fc6bb542a6e3dd166cdacc6f56d62794792d3822
Tristan Southwell <tristan.southwell@hotmail.com>
2017-09-14 22:46:50 +1000

    loging process refactored to use passport.js with facebook OAuth

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  facebookClientID: '516669255385783',
+  facebookClientSecret: '7447ea1b986da5a798f6ce70cadcbe0b',
+  mongoURI: 'mongodb://Tristan:pestacide@ds135624.mlab.com:35624/dive-buddy-dev',
+  cookieKey: 'fububfvuyweo87gvh87hwerfiubwekjhrbfkjhbwervg97h49tbfihbekjfbv'
+};
